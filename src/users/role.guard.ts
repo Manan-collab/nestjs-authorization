376d1ce812baa29d5edba4e9880e29b6878be317
manan-coditas <manan.mer@coditas.com>
2023-02-20 10:39:54 +0530

    first commit

---
+++ src/users/role.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Role } from "./entities/role.enum";
+import { User } from "./entities/user.entity";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean {
+
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (!requiredRoles) {
+            return true;
+        }
+
+        // const { user } = context.switchToHttp().getRequest();
+        const user: User = {
+            name: 'Max',
+            roles: [Role.USER],
+        }
+
+        return requiredRoles.some((role) => user.roles.includes(role));
+    }
+
+
+}
